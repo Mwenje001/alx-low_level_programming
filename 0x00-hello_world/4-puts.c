3c5a8fb62e601e3fa17bf99d87d2ff8607f196ca
Mwenje001 <mwenje72@gmail.com>
2022-09-08 08:54:16 -0700

    d

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,13 +1,8 @@
 #include <stdio.h>
 
-
-
 /**
- *  * main - Entry point
- *
- *   *
- *
- *    * Return: Always 0 (Successi)     
+ ** main - Entry point
+ ** Return: Always 0 (Successi)     
  */
 
 int main(void)
