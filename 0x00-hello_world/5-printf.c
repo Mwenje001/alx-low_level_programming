25473abb4c98d3f795b2b2b0949c3ba6d70dd420
Mwenje001 <mwenje72@gmail.com>
2022-09-08 12:05:04 -0700

    l

--- 0x00-hello_world/5-printf.c
+++ 0x00-hello_world/5-printf.c
@@ -3,14 +3,11 @@
 
 
 /**
+ *  * main  description - "with proper grammar, but the outcome is a piece of art,"
  *
- *  * main - prints the phrase with proper grammar,
- *
- *   * but the outcome is a piece of art,
  *
  *    * Return: 0 if exited properly, non-zero otherwise
- *
- *     */
+ */
 
 int main(void)
 
