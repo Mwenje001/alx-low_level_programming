5188d7aff456cf7532af7777d49a161750c3e711
Mwenje001 <mwenje72@gmail.com>
2022-09-15 02:31:44 -0700

    v

--- 0x04-more_functions_nested_loops/0-isupper.c
+++ 0x04-more_functions_nested_loops/0-isupper.c
@@ -1,17 +1,25 @@
-#include <stdio.h>
 #include "main.h"
+#include <stdio.h>
 
 
 /**
- * supper - checks for uppercase character
- * @c: the character to be checked
- * * Return: 1 if c is uppercase, 0 otherwise
+ * *_isupper - function that verifies if a  character is uppercase or not
+ * *@c: tested character
+ * *Return: returns 1 if it is uppercase , 0 if not
  */
 
- int _isupper(int c)
+
+
+int _isupper(int c)
 
 {
 
-	return (c >= 'A' && c <= 'Z');
+	if ((c >= 'A') && (c <= 'Z'))
+
+	return (1);
+
+
+
+	return (0);
 
 }
---
+++ 0x04-more_functions_nested_loops/2-mul.c
@@ -0,0 +1,22 @@
+#include "main.h"
+#include <stdio.h>
+
+
+/**
+ * es two integers
+ * @a: int to be multiplied to b
+ * * @b: int to be multiplied to a
+ * * Return: the result of the operation
+ */
+ 
+ int mul(int a, int b)
+{
+
+	int c;
+
+	c = a * b;
+
+
+	return (c);
+
+}
