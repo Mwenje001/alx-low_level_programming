8a60958cae40aae15a131f92c680dc697b95474f
Mwenje001 <mwenje72@gmail.com>
2022-09-15 02:45:55 -0700

    l

--- 0x04-more_functions_nested_loops/2-mul.c
+++ 0x04-more_functions_nested_loops/2-mul.c
@@ -1,5 +1,5 @@
 #include "main.h"
-
+#include <stdio.h>
 
 /**
  * es two integers
--- 0x04-more_functions_nested_loops/4-print_most_numbers.c
+++ 0x04-more_functions_nested_loops/4-print_most_numbers.c
@@ -1,2 +1,31 @@
 #include "main.h"
 #include <stdio.h>
+
+/**
+ * *print_most_numbers - prints numbers except 2 and 4
+ * *Return: returns nothing
+ */
+void print_most_numbers(void)
+{
+
+	int number;
+
+	for (number = 48; number < 58; number++)
+
+	{
+
+	if ((number == 50) || (number == 52))
+
+		{
+
+		continue;
+
+		}
+
+	_putchar(number);
+
+	}
+
+	_putchar(10);
+
+}
