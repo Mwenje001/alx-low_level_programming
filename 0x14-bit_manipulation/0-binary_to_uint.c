bfa4fda58f65e1cca60df193b801e5ce4c1ab3b9
Mwenje001 <mwenje72@gmail.com>
2022-11-05 16:44:46 +0300

    Correction

--- 0x14-bit_manipulation/0-binary_to_uint.c
+++ 0x14-bit_manipulation/0-binary_to_uint.c
@@ -1,90 +1,36 @@
 #include "main.h"
+
 /**
- * binary_to_uint - converts a binary to an unsigned int
- * @b: the binary number as a string
- * Return: the converted value
+ * binary_to_uint - converts a binary number to an
+ * unsigned int.
+ * @b: binary.
+ * Return: unsigned int.
  */
 unsigned int binary_to_uint(const char *b)
 {
-	unsigned int decimal = 0;
-	int str_len = 0, base = 1;
+	unsigned int i;
+	int width, bank;
 
-	if (!check_valid_string(b))
+	if (!b)
 		return (0);
 
-	while (b[str_len] != '\0')
-		str_len++;
+	i = 0;
 
-	while (str_len)
+	for (width = 0; b[width] != '\0'; width++)
+		;
+
+	for (width--, bank = 1; width >= 0; width--, bank *= 2)
 	{
-		decimal += ((b[str_len - 1] - '0') * base);
-		base *= 2;
-		str_len--;
-	}
-	return (decimal);
-}
-
-/**
- * check_valid_string - checks if a string has only 0's and 1's
- * @b: string to be checked
- * Return: 1 if string is valid, 0 otherwise
- */
-int check_valid_string(const char *b)
-{
-	if (b == NULL)
+		if (b[width] != '0' && b[width] != '1')
+		{
 		return (0);
+		}
 
-	while (*b)
-	{
-		if (*b != '1' && *b != '0')
-			return (0);
-		b++;
+		if (b[width] & 1)
+		{
+			i += bank;
+		}
 	}
-	return (1);
-}
 
-#include "main.h"
-
-/**
- * binary_to_uint - converts a binary to an unsigned int
- * @b: the binary number as a string
- * Return: the converted value
- */
-unsigned int binary_to_uint(const char *b)
-{
-	unsigned int decimal = 0;
-	int str_len = 0, base = 1;
-
-	if (!check_valid_string(b))
-		return (0);
-
-	while (b[str_len] != '\0')
-		str_len++;
-
-	while (str_len)
-	{
-		decimal += ((b[str_len - 1] - '0') * base);
-		base *= 2;
-		str_len--;
-	}
-	return (decimal);
-}
-
-/**
- * check_valid_string - checks if a string has only 0's and 1's
- * @b: string to be checked
- * Return: 1 if string is valid, 0 otherwise
- */
-int check_valid_string(const char *b)
-{
-	if (b == NULL)
-		return (0);
-
-	while (*b)
-	{
-		if (*b != '1' && *b != '0')
-			return (0);
-		b++;
-	}
-	return (1);
+	return (i);
 }
