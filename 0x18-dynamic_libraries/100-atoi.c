bca2ac977f4ae0031c9011b88d30393dec7fd976
Mwenje001 <mwenje72@gmail.com>
2022-12-13 16:42:56 +0300

    Let us See

---
+++ 0x18-dynamic_libraries/100-atoi.c
@@ -0,0 +1,40 @@
+#include "main.h"
+#include <stdio.h>
+
+/**
+ *  _atoi - int
+ *   * @s: pointer
+ *  * main - check the code for Holberton School students.
+ *  * Return: Always 0.
+ */
+
+int _atoi(char *s)
+
+{
+
+	int i;
+
+	int res = 0;
+
+	int sig = -1;
+
+	int brk = 0;
+
+	for (i = 0; s[i] != '\0'; i++)
+	{
+	if (s[i] == '-')
+		sig = sig * -1;
+	if (s[i] >= '0' && s[i] <= '9')
+		{
+		res = res * 10;
+		res -= (s[i] - '0');
+		brk = 1;
+		}
+	else if (brk == 1)
+		break;
+	}
+	res = sig * res;
+
+	return (res);
+
+}
